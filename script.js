3dce3e916143cb876a8e46a3d65e03b91aff9500
yohann <devopsy87@gmail.com>
2024-04-30 20:23:03 +0300

    first

---
+++ script.js
@@ -0,0 +1,23 @@
+/*console.log("hello")
+
+var x = 12;
+console.log(12);
+
+var xToString = x.toString();
+console.log(xToString)*/ 
+
+var speed =95;
+
+if(speed<80)
+{
+    console.log("tu roule a la bonne vitesse");
+}
+else if( speed<100)
+{
+    console.log("ta vitesse est trop eleve");
+}
+
+else
+{
+    console.log("Alors execute ce code ci");
+}
